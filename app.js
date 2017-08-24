2b27c22ff27204e4b00fc8e68fc18ff9c3e7f0c8
Dhaval Gala <dhaval.gala59@gmail.com>
2017-08-24 17:16:23 +0530

    Mongo URI

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express'),
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-var promise = mongoose.connect('mongodb://localhost/test', {
+var promise = mongoose.connect(process.env.MONGODB_URI, {
     useMongoClient: true,
 });
 promise.then(function (db) { });
