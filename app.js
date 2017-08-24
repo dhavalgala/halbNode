9a400e511637f8eef87b024a47bfcbc5727b995b
Dhaval Gala <dhaval.gala59@gmail.com>
2017-08-24 17:33:59 +0530

    log db

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ mongoose.Promise = global.Promise;
 var promise = mongoose.connect(process.env.MONGODB_URI, {
     useMongoClient: true,
 });
-promise.then(function (db) { });
+promise.then(function (db) {
+    console.log(db);
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
